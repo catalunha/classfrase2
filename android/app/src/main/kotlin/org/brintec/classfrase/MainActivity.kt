b20e788d5588b86df17bb906c4dedb02ecead260
catalunha <catalunha.mj@gmail.com>
2022-08-04 08:01:35 -0300

    initial commit

---
+++ android/app/src/main/kotlin/org/brintec/classfrase/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.brintec.classfrase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ docs/diary.md
@@ -0,0 +1,1 @@
+catalunha@pop-os:~/myapp$ flutter create --project-name=classfrase --org org.brintec --platforms android,web ./classfrase
